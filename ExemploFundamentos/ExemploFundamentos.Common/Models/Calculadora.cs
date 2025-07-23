65908a7b6ad601ef1f66958ca94f13999f1a3800
HeloSalgado <heloisa.salgado@sptech.school>
2025-07-23 20:25:29 -0300

    Docs: Adding descriptive comments to Pessoa and Calculadora classes and their methods

--- ExemploFundamentos/ExemploFundamentos.Common/Models/Calculadora.cs
+++ ExemploFundamentos/ExemploFundamentos.Common/Models/Calculadora.cs
@@ -5,34 +5,65 @@ using System.Threading.Tasks;
 
 namespace ExemploFundamentos.Common.Models
 {
+    /// <summary>
+    /// Classe que representa uma calculadora com operações matemáticas básicas.
+    /// </summary>
     public class Calculadora
     {
+        /// <summary>
+        /// Método que realiza a soma de dois números.
+        /// </summary>
+        /// <param name="x">O primeiro número inteiro</param>
+        /// <param name="y">O segundo número inteiro</param>
         public void Somar(int x, int y)
         {
             Console.WriteLine($"{x} + {y} = {x + y}");
         }
-
+        /// <summary>
+        /// Método que realiza a subtração de dois números.
+        /// </summary>
+        /// <param name="x">O primeiro número inteiro</param>
+        /// <param name="y">O segundo número inteiro</param>
         public void Sutrair(int x, int y)
         {
             Console.WriteLine($"{x} - {y} = {x - y}");
         }
 
+        /// <summary>
+        /// Método que realiza a multiplicação de dois números.
+        /// </summary>
+        /// <param name="x">O primeiro número inteiro</param>
+        /// <param name="y">O segundo número inteiro</param>
         public void Multiplicar(int x, int y)
         {
             Console.WriteLine($"{x} * {y} = {x * y}");
         }
 
+        /// <summary>
+        /// Método que realiza a divisão de dois números.
+        /// </summary>
+        /// <param name="x">O primeiro número inteiro</param>
+        /// <param name="y">O segundo número inteiro</param>
         public void Dividir(int x, int y)
         {
             Console.WriteLine($"{x} / {y} = {x / y}");
         }
 
+        /// <summary>
+        /// Método que calcula a potência de um número.
+        /// </summary>
+        /// <param name="x">O primeiro número inteiro</param>
+        /// <param name="y">O segundo número inteiro</param>
         public void Potencia(int x, int y)
         {
             double pot = Math.Pow(x, y);
             Console.WriteLine($"{x} ^ {y} = {pot}");
         }
 
+        /// <summary>
+        /// Método que calcula o seno de um ângulo em graus.
+        /// </summary>
+        /// <param name="angulo">Valor do ângulo</param>
         public void Seno(double angulo)
         {
             double radiano = Math.PI / 180 * angulo;
@@ -40,6 +71,10 @@ namespace ExemploFundamentos.Common.Models
             Console.WriteLine($"Seno de {angulo}° = {Math.Round(seno, 4)}");
         }
 
+        /// <summary>
+        /// Método que calcula o cosseno de um ângulo em graus.
+        /// </summary>
+        /// <param name="angulo">Valor do ângulo</param>
         public void Coseno(double angulo)
         {
             double radiano = Math.PI / 180 * angulo;
@@ -47,6 +82,10 @@ namespace ExemploFundamentos.Common.Models
             Console.WriteLine($"Coseno de {angulo}° = {Math.Round(coseno, 4)}");
         }
 
+        /// <summary>
+        /// Método que calcula a tangente de um ângulo em graus.
+        /// </summary>
+        /// <param name="angulo">Valor do ângulo</param>
         public void Tangente(double angulo)
         {
             double radiano = Math.PI / 180 * angulo;
@@ -54,6 +93,10 @@ namespace ExemploFundamentos.Common.Models
             Console.WriteLine($"Coseno de {angulo}° = {Math.Round(tangente, 4)}");
         }
 
+        /// <summary>
+        /// Método que calcula a raiz quadrada de um número.
+        /// </summary>
+        /// <param name="x">O número para calcular a raiz quadrada</param>
         public void RaizQuadrada(double x)
         {
             double raiz = Math.Sqrt(x);
--- ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
+++ ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
@@ -1,10 +1,16 @@
 namespace ExemploFundamentos.Common.Models
 {
+    /// <summary>
+    /// Classe que representa uma pessoa com nome e idade.
+    /// </summary>
     public class Pessoa
     {
         public string? Nome { get; set; }
         public int Idade { get; set; }
 
+        /// <summary>
+        /// Método que apresenta as informações da pessoa.
+        /// </summary>
         public void Apresentar()
         {
             Console.WriteLine($"Olá meu nome é {Nome}, e tenho {Idade} anos");
